496cc91704bb0ac56b289af585800437f57c409b
iftekhar joy <chowdhuryiftekharulislam@outlook.com>
2019-01-26 19:12:53 +0600

    modified

--- cost_management/urls.py
+++ cost_management/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('list/', views.my_expense, name='cost_list'),
+    path('list/', views.my_expense, name='costlist'),
+    path('add/', views.add_expense, name='addexpense'),
 ]
